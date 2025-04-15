0734d6dd8abcd7dac3e3676e677fc3e599bd3a0d
nanazo08 <gianajuanzo@gmail.com>
2025-04-15 19:33:00 +0800

    dyname pie chart

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -36,20 +36,41 @@ def PieCountbySeverity(request):
     GROUP BY severity_level
     '''
 
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            rows = cursor.fetchall()
-            
-        if rows:
-            data = {str(severity): int(count) for severity, count in rows}  # Ensure proper data types
-        else:
-            data = {"No Data": 100}  # Default data if no records found
-            
-        return JsonResponse(data)
-    except Exception as e:
-        print(f"Error in PieCountbySeverity: {str(e)}")  # For debugging
-        return JsonResponse({"error": "Failed to fetch data"}, status=500)
+    data = {}
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        
+    if rows:
+        # Construct the dictionary with seerity level as keys and count as values
+        data = {str(severity): int(count) for severity, count in rows}  # Ensure proper data types
+    else:
+        data = {"No Data": 100}  # Default data if no records found
+        
+    return JsonResponse(data)
+
+def LineCountbyMonth(request):
+    current_year = datetime.now().year
+
+    result = {month: 0 for month in range(1, 13)}
+
+    incidents_per_month = incident.object.filter(date_time_year=current_year) \
+        .values_list('date_time', flat=True)
+
+       # Counting the number of incidents per month
+    for date_time in incidents_per_month:
+        month = date_time.month
+        result[month] += 1
+
+    # If you want to convert month numbers to month names, youcan use a dictionary mapping
+    month_names = {
+        1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun',
+        7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'
+    }
+
+    result_with_month_names = {month_names[month]: count for month, count in result.items()}
+
+    return JsonResponse(result_with_month_names)
 
 
     
--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView, ChartView, PieCountbySeverity
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth
 from fire import views
 from fire.views import monthly_incidents
 
@@ -10,6 +10,6 @@ urlpatterns = [
     path('stations', views.map_station, name='map-station'),
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
-    path('chart', PieCountbySeverity, name='chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
     path('monthly-incidents/', monthly_incidents, name='monthly-incidents'),
 ]
